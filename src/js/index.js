9e7a5446cec2377a3e08ab97df237be59d5f874d
Lajos Meszaros <m_lajos@hotmail.com>
2020-01-08 16:58:18 +0100

    refactor(three.js): now using named imports, which should be good for treeshaking

--- src/js/index.js
+++ src/js/index.js
@@ -25,21 +25,21 @@ Promise.all([
 
 /* global requestAnimationFrame */
 
-import * as THREE from 'three'
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three'
 
 const width = 640
 const height = 480
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+const scene = new Scene()
+const camera = new PerspectiveCamera(75, width / height, 0.1, 1000)
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new WebGLRenderer()
 renderer.setSize(width, height)
 document.body.appendChild(renderer.domElement)
 
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
-const cube = new THREE.Mesh(geometry, material)
+const geometry = new BoxGeometry(1, 1, 1)
+const material = new MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new Mesh(geometry, material)
 scene.add(cube)
 
 camera.position.z = 5
