d7b79b9e0cd7dc6927654697cb290cc4da36e9d4
Frederik Gelder <frederik.gelder@freenet.de>
2020-01-26 23:20:30 +0100

    feat(editor camera controller): making camera control applicable to all objects instead of just cams

--- src/js/EditorCameraControl.ts
+++ src/js/EditorCameraControl.ts
@@ -1,4 +1,4 @@
-import { Camera, Euler, Vector3 } from 'three'
+import { Euler, Vector3, Object3D } from 'three'
 import { ThreeApp } from './ThreeApp'
 
 export class EditorCameraControl {
@@ -6,10 +6,10 @@ export class EditorCameraControl {
   public movementSpeed = 1
 
   private threeApp_: ThreeApp
-  private camera_: Camera
+  private target_: Object3D
 
   private isMouseDown = false
-  private camEuler = new Euler(0, 0, 0, 'YXZ')
+  private targetEuler = new Euler(0, 0, 0, 'YXZ')
 
   private controlButton = 2
 
@@ -29,22 +29,22 @@ export class EditorCameraControl {
 
   private movedWhileDown = false
 
-  public get camera(): Camera {
-    return this.camera_
+  public get target(): Object3D {
+    return this.target_
   }
 
   public get threeApp(): ThreeApp {
     return this.threeApp_
   }
 
-  constructor(threeApp: ThreeApp, camera: Camera) {
+  constructor(threeApp: ThreeApp, target: Object3D) {
     this.initEvents(threeApp)
 
-    this.camEuler.y = camera.rotation.y
-    this.camEuler.x = camera.rotation.x
+    this.targetEuler.y = target.rotation.y
+    this.targetEuler.x = target.rotation.x
 
     this.threeApp_ = threeApp
-    this.camera_ = camera
+    this.target_ = target
   }
 
   private initEvents(threeApp: ThreeApp): void {
@@ -64,10 +64,10 @@ export class EditorCameraControl {
   }
 
   private onAnimate = (): void => {
-    // update cam position etc
-    const forward = new Vector3(0, 0, -1).applyQuaternion(this.camera.quaternion)
-    const left = new Vector3(-1, 0, 0).applyQuaternion(this.camera.quaternion)
-    const up = new Vector3(0, 1, 0) // up down motion is not depending on camera orientation
+    // update target position etc
+    const forward = new Vector3(0, 0, -1).applyQuaternion(this.target.quaternion)
+    const left = new Vector3(-1, 0, 0).applyQuaternion(this.target.quaternion)
+    const up = new Vector3(0, 1, 0) // up down motion is not depending on target orientation
 
     const offset = new Vector3()
     if (this.forwardsDown) {
@@ -90,7 +90,7 @@ export class EditorCameraControl {
     }
 
     offset.multiplyScalar(this.movementSpeed)
-    this.camera.position.add(offset)
+    this.target.position.add(offset)
   }
 
   // prevent context menu from showing up if we moved the mouse while rightclicking
@@ -101,14 +101,14 @@ export class EditorCameraControl {
   }
 
   private onMouseMove = (ev: MouseEvent): void => {
-    // camerarotation should actually be updated in animate, but i think its fine in this case?
+    // target rotation should actually be updated in animate, but i think its fine in this case?
     if (this.isMouseDown) {
       this.movedWhileDown = true
-      this.camEuler.y -= ev.movementX * this.rotationSpeed * 0.01
-      this.camEuler.x -= ev.movementY * this.rotationSpeed * 0.01
-      this.camEuler.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, this.camEuler.x))
+      this.targetEuler.y -= ev.movementX * this.rotationSpeed * 0.01
+      this.targetEuler.x -= ev.movementY * this.rotationSpeed * 0.01
+      this.targetEuler.x = Math.max(-Math.PI / 2, Math.min(Math.PI / 2, this.targetEuler.x))
 
-      this.camera_.quaternion.setFromEuler(this.camEuler)
+      this.target_.quaternion.setFromEuler(this.targetEuler)
     }
   }
 
