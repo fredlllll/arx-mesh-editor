cb2762edb8e7007d6b9f1f0cd51ca28dd10041ee
Lajos Meszaros <m_lajos@hotmail.com>
2020-01-30 13:46:12 +0100

    feat(button): added some style to the buttons in the header

--- src/renderer/components/Header/index.tsx
+++ src/renderer/components/Header/index.tsx
@@ -12,10 +12,12 @@ const Header = (props: HeaderProps): ReactElement<any> => {
   const { onMinimizeClick, onMaximizeClick, onCloseClick } = props
   return (
     <header id={s.Header}>
+      <div className={s.right}>
+        <Button onClick={(): void => onMinimizeClick()} label="_" />
+        <Button onClick={(): void => onMaximizeClick()} label="[ ]" />
+        <Button onClick={(): void => onCloseClick()} label="&times;" />
+      </div>
       <h1>Arx Mesh Editor</h1>
-      <Button onClick={(): void => onMinimizeClick()} label="_" />
-      <Button onClick={(): void => onMaximizeClick()} label="[ ]" />
-      <Button onClick={(): void => onCloseClick()} label="x" />
     </header>
   )
 }
