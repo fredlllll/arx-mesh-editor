d9c26c2b5e140575865b46b9c058a0556c10aa40
Lajos Meszaros <m_lajos@hotmail.com>
2020-01-30 12:44:48 +0100

    refactor(button): specified proper type for the onClick property

--- src/renderer/components/Button/index.tsx
+++ src/renderer/components/Button/index.tsx
@@ -1,13 +1,12 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, MouseEventHandler } from 'react'
 import cn from 'classnames'
 import s from './style.scss'
 
 interface ButtonProps {
   label: string
-  onClick: any // TODO: I don't know what the hell I supposed to write here, MouseEvent is not enough
+  onClick: MouseEventHandler
   id?: string
   className?: string
-  // style?: ??? - string is not okay
 }
 
 const Button = (props: ButtonProps): ReactElement<any> => {
