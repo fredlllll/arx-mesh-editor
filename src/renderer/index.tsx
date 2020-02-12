b76bfdf2c92def14a89826b686a1e445f4971bc0
Lajos.Meszaros <m_lajos@hotmail.com>
2020-02-12 14:19:03 +0100

    feat(tools): enabled dev tools and reloading via F12 and F5 respectively

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,3 +1,5 @@
+/* global location */
+
 import { remote } from 'electron'
 import React from 'react'
 import { render } from 'react-dom'
@@ -9,3 +11,12 @@ if (container !== null) {
   const app = <App electronRemote={remote} />
   render(app, container)
 }
+
+document.addEventListener('keydown', (e): void => {
+  if (e.code === 'F12') {
+    remote.getCurrentWindow().webContents.toggleDevTools()
+  }
+  if (e.code === 'F5') {
+    location.reload()
+  }
+})
