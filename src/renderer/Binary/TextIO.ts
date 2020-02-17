156c6282c16d96cc2ab46edd27e38fbec068b13c
Lajos Meszaros <m_lajos@hotmail.com>
2020-02-17 17:32:42 +0100

    refactor(textio): added a fallback character and byte for out of bounds coverage

--- src/renderer/Binary/TextIO.ts
+++ src/renderer/Binary/TextIO.ts
@@ -1,7 +1,8 @@
-import { map, prop, __, join, has } from 'ramda'
+import { map, join, propOr, split, nth, defaultTo } from 'ramda'
 import { CHARS, CODES } from './Latin1CharsetLookup'
 
-const UNKNOWN_CHAR = CODES[' ']
+const BYTE_OF_AN_UNKNOWN_CHAR = CODES[' ']
+const CHAR_OF_AN_UNKNOWN_BYTE = ' '
 
 class TextIO {
   charset: string
@@ -11,11 +12,14 @@ class TextIO {
   }
 
   public decode(bytes: number[]): string {
-    return join('', map(prop(__, CHARS), bytes))
+    return join(
+      '',
+      map(byte => defaultTo(CHAR_OF_AN_UNKNOWN_BYTE, nth(byte, CHARS)), bytes)
+    )
   }
 
   public encode(str: string): number[] {
-    return str.split('').map(char => (has(char, CODES) ? CODES[char] : UNKNOWN_CHAR))
+    return map(char => propOr(BYTE_OF_AN_UNKNOWN_CHAR, char, CODES), split('', str))
   }
 }
 
