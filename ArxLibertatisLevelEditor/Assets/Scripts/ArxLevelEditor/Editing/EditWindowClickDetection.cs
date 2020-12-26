23df07b66c1e32c273120079cc46b98a67b63356
Frederik Gelder <frederik.gelder@freenet.de>
2020-12-26 02:24:03 +0100

    adding a universal click and drag handling class to be used with edit window

---
+++ ArxLibertatisLevelEditor/Assets/Scripts/ArxLevelEditor/Editing/EditWindowClickDetection.cs
@@ -0,0 +1,188 @@
+﻿using Assets.Scripts.Util;
+using System;
+using UnityEngine;
+
+namespace Assets.Scripts.ArxLevelEditor.Editing
+{
+    using ClickHandler = Func<Vector3, int, bool>;
+    using MouseDownHandler = Func<Vector3, int, bool>;
+    using MouseUpHandler = Func<Vector3, int, bool, bool>;
+    using BeginDragHandler = Func<Vector3, int, bool>;
+    using DragHandler = Func<Vector3, Vector3, Vector3, int, bool>;
+    using EndDragHandler = Func<Vector3, int, bool>;
+
+    public class EditWindowClickDetection : MonoBehaviour
+    {
+        public const int BTN_PRIMARY = 0;
+        public const int BTN_SECONDARY = 1;
+        public const int BTN_MIDDLE = 2;
+
+        public static EditWindowClickDetection Instance { get; private set; } = null;
+
+        public static PriorityList<ClickHandler> clickHandlers = new PriorityList<ClickHandler>();
+        public static PriorityList<MouseDownHandler> mouseDownHandlers = new PriorityList<MouseDownHandler>();
+        public static PriorityList<MouseUpHandler> mouseUpHandlers = new PriorityList<MouseUpHandler>();
+        public static PriorityList<BeginDragHandler> beginDragHandlers = new PriorityList<BeginDragHandler>();
+        public static PriorityList<DragHandler> dragHandlers = new PriorityList<DragHandler>();
+        public static PriorityList<EndDragHandler> endDragHandlers = new PriorityList<EndDragHandler>();
+
+        private static Vector3[] lastPosition = new Vector3[3];
+        private static Vector3[] mouseDownPosition = new Vector3[3];
+        private static bool[] mouseButtonDown = new bool[] { false, false, false };
+        private static bool[] mouseButtonMoved = new bool[] { false, false, false };
+        private static bool[] isDragging = new bool[] { false, false, false };
+
+        public float distanceTillDrag = 3; //3 pixels of movement till it actually counts as drag
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                throw new Exception("Only one instance of edit window click detection allowed");
+            }
+            Instance = this;
+            Vector3 posLocal = MouseGlobalToLocal(Input.mousePosition);
+            for (int i = 0; i < lastPosition.Length; i++)
+            {
+                lastPosition[i] = posLocal;
+            }
+        }
+
+        private Vector3 MouseGlobalToLocal(Vector3 globalPos)
+        {
+            globalPos.x -= EditWindow.X;
+            globalPos.y -= EditWindow.Y;
+            return globalPos;
+        }
+
+        private void FireEvent<T1, T2>(PriorityList<Func<T1, T2, bool>> prioList, T1 t1, T2 t2)
+        {
+            foreach (var prio in prioList.GetPriorities())
+            {
+                foreach (var handler in prioList.GetPriorityItems(prio))
+                {
+                    if (handler(t1, t2))
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void FireEvent<T1, T2, T3>(PriorityList<Func<T1, T2, T3, bool>> prioList, T1 t1, T2 t2, T3 t3)
+        {
+            foreach (var prio in prioList.GetPriorities())
+            {
+                foreach (var handler in prioList.GetPriorityItems(prio))
+                {
+                    if (handler(t1, t2, t3))
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void FireEvent<T1, T2, T3, T4>(PriorityList<Func<T1, T2, T3, T4, bool>> prioList, T1 t1, T2 t2, T3 t3, T4 t4)
+        {
+            foreach (var prio in prioList.GetPriorities())
+            {
+                foreach (var handler in prioList.GetPriorityItems(prio))
+                {
+                    if (handler(t1, t2, t3, t4))
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void FireMouseDown(Vector3 mousePosLocal, int mouseButton)
+        {
+            FireEvent(mouseDownHandlers, mousePosLocal, mouseButton);
+        }
+
+        private void FireMouseUp(Vector3 mousePosLocal, int mouseButton, bool insideEditWindow)
+        {
+            FireEvent(mouseUpHandlers, mousePosLocal, mouseButton, insideEditWindow);
+        }
+
+        private void FireClick(Vector3 mousePosLocal, int mouseButton)
+        {
+            FireEvent(clickHandlers, mousePosLocal, mouseButton);
+        }
+
+        private void FireBeginDrag(Vector3 mousePosLocal, int mouseButton)
+        {
+            FireEvent(beginDragHandlers, mousePosLocal, mouseButton);
+        }
+
+        private void FireDrag(Vector3 lastPos, Vector3 nowPos, Vector3 offset, int mouseButton)
+        {
+            FireEvent(dragHandlers, lastPos, nowPos, offset, mouseButton);
+        }
+
+        private void FireEndDrag(Vector3 mousePosLocal, int mouseButton)
+        {
+            FireEvent(endDragHandlers, mousePosLocal, mouseButton);
+        }
+
+        private void UpdateButton(int btn)
+        {
+            var mousePosLocal = MouseGlobalToLocal(Input.mousePosition);
+            if (EditWindow.MouseInEditWindow)
+            {
+                if (Input.GetMouseButtonDown(btn))
+                {
+                    FireMouseDown(mousePosLocal, btn);
+                    mouseButtonDown[btn] = true;
+                    mouseButtonMoved[btn] = false;
+                    mouseDownPosition[btn] = mousePosLocal;
+                }
+            }
+            if (Input.GetMouseButtonUp(btn))
+            {
+                FireMouseUp(mousePosLocal, btn, EditWindow.MouseInEditWindow);
+                if (EditWindow.MouseInEditWindow && mouseButtonDown[btn] && !mouseButtonMoved[btn])
+                {
+                    FireClick(mousePosLocal, btn);
+                }
+                mouseButtonDown[btn] = false;
+                if (isDragging[btn])
+                {
+                    FireEndDrag(mousePosLocal, btn);
+                    isDragging[btn] = false;
+                }
+            }
+
+            if (Input.GetMouseButton(btn))
+            {
+                if (isDragging[btn])
+                {
+                    var offset = mousePosLocal - lastPosition[btn];
+                    FireDrag(lastPosition[btn], mousePosLocal, offset, btn);
+                }
+                else
+                {
+                    var offsetSinceDown = mousePosLocal - mouseDownPosition[btn];
+                    var distanceSinceDown = offsetSinceDown.magnitude;
+                    if (distanceSinceDown >= distanceTillDrag)
+                    {
+                        FireBeginDrag(mousePosLocal, btn);
+                        isDragging[btn] = true;
+                        FireDrag(mouseDownPosition[btn], mousePosLocal, offsetSinceDown, btn);
+                    }
+                }
+            }
+
+            lastPosition[btn] = mousePosLocal;
+        }
+
+        private void Update()
+        {
+            UpdateButton(BTN_PRIMARY);
+            UpdateButton(BTN_SECONDARY);
+            UpdateButton(BTN_MIDDLE);
+        }
+    }
+}
---
+++ ArxLibertatisLevelEditor/Assets/Scripts/Util/PriorityList.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Assets.Scripts.Util
+{
+    public class PriorityList<T>
+    {
+        readonly Dictionary<int, List<T>> items = new Dictionary<int, List<T>>();
+        readonly List<int> priorities = new List<int>();
+
+        public void Add(T item, int priority)
+        {
+            if (!items.TryGetValue(priority, out List<T> lst))
+            {
+                lst = new List<T>();
+                items[priority] = lst;
+                priorities.Add(priority);
+                priorities.Sort((a, b) => b.CompareTo(a)); //descending order list
+            }
+
+            lst.Add(item);
+        }
+
+        public bool Remove(T item, int priority)
+        {
+            if (items.TryGetValue(priority, out List<T> lst))
+            {
+                return lst.Remove(item);
+            }
+            return false;
+        }
+
+        public bool Remove(T item)
+        {
+            bool retval = false;
+            foreach (var prio in priorities)
+            {
+                retval = Remove(item, prio) || retval; //written in this order cause second parameter in OR is not executed if first param is true
+            }
+            return retval;
+        }
+
+        public IEnumerable<int> GetPriorities()
+        {
+            return priorities;
+        }
+
+        public IEnumerable<T> GetPriorityItems(int priority)
+        {
+            if (items.TryGetValue(priority, out List<T> lst))
+            {
+                return lst;
+            }
+            return Enumerable.Empty<T>();
+        }
+
+        public void Clear()
+        {
+            priorities.Clear();
+            items.Clear();
+        }
+    }
+}
