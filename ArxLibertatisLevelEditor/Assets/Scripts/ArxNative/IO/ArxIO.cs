59b3312b0a1a194cf71ec76741b2bffa99bdeab0
Frederik Gelder <frederik.gelder@freenet.de>
2021-05-04 01:27:15 +0200

    adding dlls for implode

--- ArxLibertatisLevelEditor/Assets/Scripts/ArxNative/IO/ArxIO.cs
+++ ArxLibertatisLevelEditor/Assets/Scripts/ArxNative/IO/ArxIO.cs
@@ -30,14 +30,27 @@ namespace Assets.Scripts.ArxNative.IO
 
         public static byte[] Pack(byte[] bytes)
         {
-            return bytes; //because arxio doesnt support that yet im just gonna save uncompressed
+            uint inSize = (uint)bytes.Length;
+
+            var pinnedBytes = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+
+            ArxIONative.PackAlloc(pinnedBytes.AddrOfPinnedObject(), inSize, out IntPtr outPtr, out uint outSize);
+
+            pinnedBytes.Free();
+
+            var outBytes = new byte[outSize];
+            Marshal.Copy(outPtr, outBytes, 0, (int)outSize);
+
+            ArxIONative.PackFree(outPtr);
+
+            return outBytes;
+
+            //return bytes; //because arxio doesnt support that yet im just gonna save uncompressed
         }
     }
 
     public static class ArxIONative
     {
-        //TODO: hook arxio.dll
-
         [DllImport("ArxIO", EntryPoint = "ArxIO_init", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Init();
         [DllImport("ArxIO", EntryPoint = "ArxIO_getError", CallingConvention = CallingConvention.Cdecl)]
@@ -48,5 +61,10 @@ namespace Assets.Scripts.ArxNative.IO
         public static extern void UnpackAlloc(IntPtr in_, uint inSize, out IntPtr out_, out uint outSize);
         [DllImport("ArxIO", EntryPoint = "ArxIO_unpack_free", CallingConvention = CallingConvention.Cdecl)]
         public static extern void UnpackFree(IntPtr buffer);
+
+        [DllImport("ArxLibertatisEditorIO", EntryPoint = "ArxIO_pack_alloc", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void PackAlloc(IntPtr in_, uint inSize, out IntPtr out_, out uint outSize);
+        [DllImport("ArxLibertatisEditorIO", EntryPoint = "ArxIO_pack_free", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void PackFree(IntPtr buffer);
     }
 }
